ad8c40267bc42545de3f8d342cce4337a4e0b700
xHunorr <111045675+xHunorr@users.noreply.github.com>
2025-11-21 02:32:55 +0100

    Update verify-code.js

--- api/verify-code.js
+++ api/verify-code.js
@@ -1,6 +1,11 @@
 import { codes } from './send-code';
 
 export default function handler(req, res) {
+
+  res.setHeader('Access-Control-Allow-Origin', 'https://rrvapes.com');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
